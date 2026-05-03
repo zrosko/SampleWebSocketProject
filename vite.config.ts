803224883aca48ef88e7d4b2f8724f8e91851dee
zdravko rosko <zrosko@gmail.com>
2026-05-02 22:05:09 -0400

    Change vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['sockjs-client', '@stomp/stompjs'],
   },
 });
+
